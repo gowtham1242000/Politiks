8f6083cdc7e5674f21c644053297569d9ae013a7
unknown <gowthamk1242000@gmail.com>
2024-07-01 10:43:31 +0530

    changes in post model page

--- models/Post.js
+++ models/Post.js
@@ -27,6 +27,9 @@ const Post = sequelize.define('Post', {
   likeCount: {
     type: DataTypes.INTEGER,
     defaultValue: 0, // Default value for like count
+},originalPostId: {
+  type: DataTypes.INTEGER,
+  allowNull: true, // This will be null for original posts, and reference the original post for reposts
 },
 
 });
